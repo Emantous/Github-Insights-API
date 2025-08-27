b6cbadfc37821153b3a5d7e52d005c6fcd69a41f
Šimon Bolemant <bolemantsimon@gmail.com>
2025-08-27 21:37:09 +0200

    Updated some API functions

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from enum import Enum
 from pydantic import BaseModel
 from fastapi import FastAPI
 import os
+import requests
 
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
@@ -25,16 +27,73 @@ def health_check():
         "version": os.getenv("APP_VERSION", "0.1.0")
     }
 
-'''@app.get("/items/{item_id}")
-async def read_item(item_id: str, q: str | None = None):
-    if q:
-        return {"item_id": item_id, "q": q}
-    return {"item_id": item_id}
+@app.get("/{owner}/{repo}/languages")
+def repo_language(owner: str, repo: str):
+    response = requests.get(f"https://api.github.com/repos/{owner}/{repo}/languages", headers={"accept" : "application/vnd.github+json"})
+    data = response.json()
+    if response.status_code == 200:
+        return data
+    else:
+        return 'heh'
 
-@app.post("/items/")
-async def create_item(item: Item):
-    item_dict = item.dict()
-    if item.tax is not None:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict'''
+@app.get("/{owner}/{repo}/contributors")
+def repo_contributors(owner: str, repo: str):
+    response = requests.get(f"https://api.github.com/repos/{owner}/{repo}/contributors", headers={"accept" : "application/vnd.github+json"})
+    data = response.json()
+    if response.status_code == 200:
+        transofrmed_contributors = []
+        for member in data:
+            transofrmed_contributors.append((member['login'], member['contributions']))
+        return transofrmed_contributors
+    else:
+        return 'heh'
+
+@app.get("/{owner}/{repo}/overview")
+def repo_overview(owner: str, repo: str):
+    response = requests.get(f"https://api.github.com/repos/{owner}/{repo}", headers={"accept" : "application/vnd.github+json"})
+    data = response.json()
+    if response.status_code == 200:
+        repo_info = {}
+        repo_info["stars"] = data["stargazers_count"]
+        repo_info["watchers"] = data["watchers_count"]
+        repo_info["main_language"] = data["language"]
+        repo_info["forks"] = data["forks_count"]
+        repo_info["open issues"] = data["open_issues_count"]
+        return repo_info
+    else:
+        return 'heh'
+
+@app.get("/{owner}/{repo}/stars")
+def repo_stars(owner: str, repo: str):
+    response = requests.get(f"https://api.github.com/repos/{owner}/{repo}/stargazers", headers={"accept" : "application/vnd.github+json"})
+    data = response.json()
+    if response.status_code == 200:
+        return data
+    else:
+        return 'heh'
+
+@app.get("/{owner}/{repo}/issues")
+def repo_issues(owner: str, repo: str):
+    response = requests.get(f"https://api.github.com/repos/{owner}/{repo}/issues", headers={"accept" : "application/vnd.github+json"})
+    data = response.json()
+    if response.status_code == 200:
+        return data
+    else:
+        return 'heh'
+
+@app.get("/{owner}/{repo}/issues/{number}")
+def repo_issue_timeline(owner: str, repo: str, number: int):
+    response = requests.get(f"https://api.github.com/repos/{owner}/{repo}/issues/{number}/timeline", headers={"accept" : "application/vnd.github+json"})
+    data = response.json()
+    if response.status_code == 200:
+        return data
+    else:
+        return 'heh'
+
+@app.post("/analyze")
+def repo_overview(owner: str, repo: str):
+    return f"Get repo {repo} from {owner}"
+
+@app.delete("/{owner}/{repo}")
+def repo_overview(owner: str, repo: str):
+    return f"Get repo {repo} from {owner}"
